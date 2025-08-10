737510cfd85a255cccc2888919e1ea9a6b506766
DivayJainDJ <divayjain1208@gmail.com>
2025-08-10 23:00:18 +0530

    Fix fetchNews to use search query parameter

--- netlify/functions/fetchNews.js
+++ netlify/functions/fetchNews.js
@@ -3,11 +3,13 @@ const fetch = require('node-fetch');
 exports.handler = async function(event, context) {
   const API_KEY = process.env.NEWS_API_KEY;
   const query = event.queryStringParameters?.query || 'technology';
-  const url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${API_KEY}`;
+
+  const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&apiKey=${API_KEY}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
+
     return {
       statusCode: 200,
       body: JSON.stringify(data)
@@ -19,3 +21,4 @@ exports.handler = async function(event, context) {
     };
   }
 };
+
